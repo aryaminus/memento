52f3cf14b46245a3dde008c7a85a2bae46bb0727
Sunim Acharya <sunim.54@gmail.com>
2018-04-29 12:56:55 +0545

    Add Pip layout

---
+++ README.rst
@@ -0,0 +1,6 @@
+To use (with caution), simply do::
+
+    $ pip install memento
+    $ mementor <dirname>
+
+Make sure you have a OCR tool like `tesseract` and certain data value for comparing OCR, eg `tesseract-data-eng` along with `Pillow` and `Wand` for image conversion and loading to draw along with `numpy` and `cv2` for all Image processing required to crop the image with border values defined which will be fetched during pip install.
---
+++ setup.py
@@ -0,0 +1,28 @@
+#from distutils.core import setup
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name = 'mementor',
+    packages = ['mementor'], # this must be the same as the name above
+    version = '1.0.0',
+    description = 'A library to fetch images from directory to fix orientation and pull OCR from the images along with editing the text inside images, primarily focusing on memes',
+    long_description = readme(),
+    author = 'Sunim Acharya',
+    author_email = 'sunim.54@gmail.com',
+    url = 'https://github.com/aryaminus/memento', # use the URL to the github repo
+    keywords = ['ocr', 'image', 'opencv', 'tesseract', 'orientation'], # arbitrary keywords
+    classifiers = [],
+    install_requires=[
+        'pillow', 'pyocr' ,'tesseract', 'numpy'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+    'console_scripts': [
+        'memento = memento.memento:start'
+    ]},
+)
